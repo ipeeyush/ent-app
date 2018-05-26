53d8766040569b99f4a3e2e61fdfcd4d414a116c
ipeeyush <peeyushthefirst@gmail.com>
2018-05-26 21:42:50 +0530

    adding model outputting and displaying the data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A test Recipe', 'This is the Description', 'https://www.inspiredtaste.net/wp-content/uploads/2012/02/Easy-Chicken-Ragu-1200.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A test Recipe', 'This is the Description', 'https://www.inspiredtaste.net/wp-content/uploads/2012/02/Easy-Chicken-Ragu-1200.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
